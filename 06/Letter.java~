c085491e4b775b9d0688b2d764871b9be3feea27
MingzeJin <36276890+MingzeJin@users.noreply.github.com>
2018-02-15 13:03:35 -0500

    Add files via upload

---
+++ 06/Letter.java~
@@ -0,0 +1,15 @@
+public class Letter {
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                      Mingze Jin         |");
+        System.out.println("|                                      23 Sycamore Dr.    |");
+        System.out.println("|                                      L3T 5V3            |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
---
+++ 08/NumberAndMath.java
@@ -0,0 +1,45 @@
+public class NumberAndMath {
+    public static void main(String[] args) {
+        // prints message
+        System.out.println("I will now count my chickens:");
+
+        // calculates and prints number of hens
+        System.out.println("Hens " + (25.0 + 30.0 / 6.0));
+
+        // calculates and prints number of roosters
+        System.out.println("Roosters " + (100.0 - 25.0 * 3.0 % 4.0));
+
+        // prints message
+        System.out.println("Now I will count the eggs:");
+
+        // calculates and prints number of eggs
+        System.out.println(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0);
+
+        // prints message
+        System.out.println("Is it true that 3 + 2 < 5 - 7?");
+
+        // checks if boolean statement is true or false
+        System.out.println(3.0 + 2.0 < 5.0 - 7.0);
+
+        // calculates sum and prints message
+        System.out.println("What is 3 + 2? " + (3.0 + 2.0));
+
+        // calculates sum and prints message
+        System.out.println("What is 5 - 7? " + (5.0 - 7.0));
+
+        // prints message
+        System.out.println("Oh, that's why it's false.");
+
+        // prints message
+        System.out.println("How about some more.");
+
+        // prints message and checks if boolean statement is true or false
+        System.out.println("Is it greater? " + (5.0 > -2.0));
+
+        // prints message and checks if boolean statement is true or false
+        System.out.println("Is it greater or equal? " + (5.0 >= -2.0));
+
+        // prints message and checks if boolean statement is true or false
+        System.out.println("Is it less or equal? " + (5.0 <= -2.0));
+    }
+}
