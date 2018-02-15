2c696a685e2ae3ecf8d9f29286e9b728ceebbeec
MingzeJin <36276890+MingzeJin@users.noreply.github.com>
2018-02-15 13:01:47 -0500

    Add files via upload

---
+++ 03/FirstProg.java
@@ -0,0 +1,9 @@
+public class FirstProg {
+    public static void main(String[] args) {
+        System.out.println( "I got" );       
+        for (int x = 1; x < 8; x += 1) {
+            System.out.println(x);
+        }
+        System.out.println("8 m's in my bank account.");
+    }
+}
---
+++ 07/Initials.java
@@ -0,0 +1,11 @@
+public class Initials {
+    public static void main(String[] args) {
+        System.out.println("M   M  JJJJJ");
+        System.out.println("MM MM    J  ");
+        System.out.println("MM MM    J  ");
+        System.out.println("M M M    J  ");
+        System.out.println("M   M  J J  ");
+        System.out.println("M   M  J J  ");
+        System.out.println("M   M   JJ  ");
+    }
+}
