84cf098a432b92999ef9550059b0d9ee432ae4ad
MingzeJin <36276890+MingzeJin@users.noreply.github.com>
2018-02-21 14:00:43 -0500

    Add files via upload

---
+++ 18/AgeInFiveYears.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class AgeInFiveYears {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Hello, what's your name? ");
+      String name = input.nextLine();
+      System.out.println("");
+        
+      System.out.print("Hi, " + name + "! How old are you? ");
+      int age = input.nextInt();
+      System.out.println("");
+      
+      if (age >= 100) { 
+          System.out.println("Did you know that in five years you'll probably be dead?");
+          System.out.println("And that five years ago you were " + (age - 5) + "?");
+      } else if (age <= 5) {
+          System.out.println("Did you know that in five years you'll be " + (age + 5) + "?");
+          System.out.println("And that five years ago you weren't born?");
+      } else {
+          System.out.println("Did you know that in five years you'll be " + (age + 5) + "?");
+          System.out.println("And that five years ago you were " + (age - 5) + "?");
+      }
+    }
+}
